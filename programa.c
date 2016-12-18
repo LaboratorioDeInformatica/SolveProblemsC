56e8d7af83af2b4f2127b43f2b8f1e58b7e2ef4b
LaboratorioDeInformatica <labdeinf@gmail.com>
2016-12-18 12:25:30 -0200

    Correção do primeiro programa

--- programa.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-	//imprime cabeçalho do jogo
-	printf("*****************************************\n");
-	printf("* Bem vindo ao nosso jogo de advinhacao *\n");
-	printf("*****************************************\n");
-}
