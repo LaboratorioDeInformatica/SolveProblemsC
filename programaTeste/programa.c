7ef6371802be51b00154acfee155bf2715e7aa19
LaboratorioDeInformatica <labdeinf@gmail.com>
2016-12-18 12:30:19 -0200

    Organização de Pastas

---
+++ programaTeste/programa.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+	//imprime cabeçalho do jogo
+	printf("*****************************************\n");
+	printf("* Bem vindo ao nosso jogo de advinhacao *\n");
+	printf("*****************************************\n");
+}
