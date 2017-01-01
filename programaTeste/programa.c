844b2505824bc309e11e39190406c2f68739eb36
LaboratorioDeInformatica <labdeinf@gmail.com>
2017-01-01 00:22:08 -0200

    correção 0

--- programaTeste/programa.c
+++ programaTeste/programa.c
@@ -4,32 +4,55 @@
 
 
 int main(){
-	//imprime cabeçalho do jogo
-	printf("*****************************************\n");
-	printf("* Bem vindo ao nosso jogo de advinhacao *\n");
-	printf("*****************************************\n");
-	
-	
+
+	int acertou = 0;
 	int segundos=time(0);
 	srand(segundos);
 	
 	int numero = rand();
 	
 	int numerosecreto = numero % 100 ;
+	
+	int numerotentativas ;
 
 	int chute ;
 	
 	int tentativas = 1 ;
-	
+	int nivel ;
 	double pontos = 1000;
 	double pontosperdidos ;
 	
+	//imprime cabeçalho do jogo
+	printf("*****************************************\n");
+	printf("* Bem vindo ao nosso jogo de advinhacao *\n");
+	printf("*****************************************\n");
 	
-	while(1){
+	printf("Escolha um nivel de dificuldade\n");
+	printf("1- facil , 2-medio , 3- difcil\n");
+	printf("Escolha:");
+	scanf("%d",&nivel);
+	
+	switch(nivel){
+	
+		case 1:
+		numerotentativas = 10 ;
+		break;
+		
+		case 2 :
+		numerotentativas = 7 ;
+		break;
+		
+		default :
+		numerotentativas = 5 ;
+		break;
+	}
+	
+
+	for(int i = 0 ; i< numerotentativas ; i++){
 		printf("Tentativa  %d \n", tentativas);
 		printf("Quel eh seu chute?\n" );
 		scanf("%d", &chute);
-		int acertou =chute == numerosecreto; 
+		 acertou =chute == numerosecreto; 
 		int menor = chute < numerosecreto ;
 		
 		if(chute < 0 ){
@@ -38,8 +61,6 @@ int main(){
 		}
 		
 		if(acertou){
-			printf("Parabens, voce acertou\n");
-			printf("Voce acertou na %d tentativa\n", tentativas);
 			break;
 		}else if(menor){
 				printf("Infelizmente, voce errou\n");
@@ -58,5 +79,11 @@ int main(){
 		
 	}
 	
-	printf("Total de pontos %.2f", pontos);
+		if(acertou){
+		printf("Parabens, voce acertou\n");
+		printf("Voce acertou na %d tentativa\n", tentativas);
+		printf("Total de pontos %.2f", pontos);
+		}else{
+			printf("Infelizmente voce perdeu\n");
+		}
 }
